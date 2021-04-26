c183d0369c92778fd076cba1c3279b888885bc04
Vituz <vtbattaglia@gmail.com>
2021-04-26 17:10:39 +0200

    Complete Mail Validato

--- mail/assets/js/main.js
+++ mail/assets/js/main.js
@@ -6,20 +6,19 @@ console.log(userMail);
 
 document.getElementById("user_mail").innerHTML = userMail;
 
-// Controllare se è presente nella lista
-var mailList = ["snoi@hotmail.it", "prova@gmail.com", "mail@libero.it"];
+// Controllare se è presente nella lista e comunicare esito della convalida
+var mailList = ["prova@gmail.com", "snoi@hotmail.it", "mail@libero.it"];
 
 console.log(mailList);
 
 for(var i = 0; i < mailList.length; i++){
-    
-    
 
-    if(mailList[i] == userMail){
+    if(mailList[i] === userMail){
         message.innerHTML = "Email presente nella Mail List!"
+        break;
+
     } else {
         message.innerHTML = "Email non presente nella Mail List!" 
     }
-}
 
-// Comunicare esito della convalida
+}
